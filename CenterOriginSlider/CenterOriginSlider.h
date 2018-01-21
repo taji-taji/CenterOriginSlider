8f0845b95014d5ea3ea646b850ac43edcb2d66e9
Yutaka Tajika <y.tajika529@gmail.com>
2018-01-21 21:17:41 +0900

    Initial Commit

---
+++ CenterOriginSlider/CenterOriginSlider.h
@@ -0,0 +1,19 @@
+//
+//  CenterOriginSlider.h
+//  CenterOriginSlider
+//
+//  Created by 多鹿豊 on 2018/01/21.
+//  Copyright © 2018年 多鹿豊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CenterOriginSlider.
+FOUNDATION_EXPORT double CenterOriginSliderVersionNumber;
+
+//! Project version string for CenterOriginSlider.
+FOUNDATION_EXPORT const unsigned char CenterOriginSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CenterOriginSlider/PublicHeader.h>
+
+
